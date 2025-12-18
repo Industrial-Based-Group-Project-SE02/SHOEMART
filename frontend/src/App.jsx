0a48a2bf9b6c0c7cfaa819e611052b27f6b13c0c
dilshanlnbti <dilshan.bsc.se02@edu.lnbti.lk>
2025-12-19 00:52:36 +0530

    Make register Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/client-page/*" element={<ClientPage />} />
           <Route path="/login" element={<Login  />} />
+          <Route path="/register" element={<Register  />} />
           
         </Routes>
       </Router>
